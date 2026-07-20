d3ab117ac3492ec17f50ef164dfe50deddac14d1
naresh-palle <npallegoud@gmail.com>
2026-07-20 23:29:37 +0530

    UI: Fix z-index stacking context blocking Nav clicks on Splash screen

--- frontend/src/pages/RegisterSplash.jsx
+++ frontend/src/pages/RegisterSplash.jsx
@@ -17,7 +17,7 @@ export default function RegisterSplash() {
         <div className="grain" />
       </div>
 
-      <div className="relative z-10">
+      <div className="relative z-50">
         <Nav />
       </div>
 
